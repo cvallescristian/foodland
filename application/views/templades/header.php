0a11eab9c3348e0b122f39d767b9a823fd271cb9
Cristian <cristian.valles.12@gmail.com>
2013-06-09 01:19:47 -0400

    Revert "Merge branch 'tester' of https://github.com/stilexmagic/foodland into tester"
    This reverts commit ca5382db62e9bab5559f366addd9f32d3ecd8303, reversing
    changes made to 602468744869fa8213a0a3f8c0916ef03441924d.

--- application/views/templades/header.php
+++ application/views/templades/header.php
@@ -1,94 +1,31 @@
-<!DOCTYPE html>
-<!-- saved from url=(0059)http://uexel.us/themes/purity/multipage/wide/portfolio.html -->
-<html lang="en" style="overflow-y: hidden;">
+<!doctype html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Bienvenido a Foodland</title>
+	
+	 <!-- Styles -->
+    <link href="<?= base_url();?>css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/theme.css">
+    <link rel="stylesheet" href="<?= base_url();?>css/index.css" type="text/css" media="screen">
 
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        
+    <link href="<?= base_url();?>css/css.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url();?>css/animate.css" rel="stylesheet" type="text/css">
+        <!--[
+    <link href="http://responsivewebinc.com/premium/metroman/style/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://responsivewebinc.com/premium/metroman/style/font-awesome.css">
+    -->
+    <link rel="stylesheet" href="<?= base_url();?>css/slider-main.css">
+    <link href="<?= base_url();?>css/isotope.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url();?>css/style.css" rel="stylesheet" type="text/css">
+    <link href=".<?= base_url();?>css/style-responsive.css" rel="stylesheet" type="text/css">
+    <link href=".<?= base_url();?>css/font-awesome.css" media="all" rel="stylesheet" type="text/css">
+   	 <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/jquery.fancybox.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/jquery.fancybox-thumbs.css">
+    <link href="<?= base_url();?>css/isotope.css" rel="stylesheet" type="text/css">
 
-
-
-       
-
-        <!--Le HTML5 shim, for IE6-8 support of HTML5 elements--> 
-        <!--[if lt IE 9]>
-          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-        <!-- Le styles -->
-        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/theme.css">
-        <link rel="stylesheet" href="<?= base_url();?>css/index.css" type="text/css" media="screen">
-        <link href="<?= base_url();?>css/css.css" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="http://responsivewebinc.com/premium/metroman/style/font-awesome.css">
-
-        <link rel="stylesheet" href="<?= base_url();?>css/slider-main.css">
-        <link href="<?= base_url();?>css/bootstrap.css" rel="stylesheet" type="text/css">
-        <link href="<?= base_url();?>css/font-awesome.css" media="all" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/jquery.fancybox.css" media="all">
-        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/jquery.fancybox-thumbs.css">
-        <link href="<?= base_url();?>css/animate.css" rel="stylesheet" type="text/css">
-        <link href="<?= base_url();?>css/isotope.css" rel="stylesheet" type="text/css">
-        <link href="<?= base_url();?>css/style.css" rel="stylesheet" type="text/css">
-        <link href="<?= base_url();?>css/style-responsive.css" rel="stylesheet" type="text/css">
-       
-
-    
-   
-    
 </head>
+<body class="pull_top">
 
-    <body>
-
-    <div class="navbar transparent navbar-inverse navbar-fixed-top" style="height:45px;">
-      <div class="navbar-inner" style="height:45px;">
-         <a class="span3 offset2" href="admin_inicio.html" style="position:relative; left:20px;">
-                <img src="<?= base_url();?>img/logo2.png" width="200" height="40"> <sup style="color:white;">2.0</sup>
-            </a>     
-        <div class="container">
-          <div class="nav-collapse">
-                <ul class="nav pull-right" style="position:relavite; top:-10px;">
-                        
-                        <li><a href="" class="active">INICIO</a></li>
-                        <li><a href="">MAPA LOCALES</a></li>
-                        <li class="dropdown">
-                            <a href="http://cleancanvas.herokuapp.com/#" class="dropdown-toggle" data-toggle="dropdown">
-                                PRODUCTOS
-                                <b class="caret"></b>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a href="">Pizza</a></li>
-                                <li><a href="">Sushi</a></li>
-                                <li><a href="">China</a></li>
-                            
-                            </ul>
-                        </li>
-                        
-                    
-            
-                   <li class="dropdown">
-                        <a href="" class="dropdown-toggle" data-toggle="dropdown"><font><font>Iniciar Sesión</font></font><b class="caret"></b></a>
-                        <div class="dropdown-menu" style="padding: 15px; color:white;" >
-                            <div class="span3">
-                                <div >
-                                    <font><font>Iniciar con:</font></font>
-                                        <h4> <a href=""> <i class="icon-twitter"></i></a>
-                                            <a href=""><i class="icon-facebook"></i></a> 
-                                           <a href=""><i class="icon-google-plus"></i> </a></h4>
-                                    
-                                </div>
-                            </div>
-                            <form action="" method="post" accept-charset="UTF-8" class="form-menu">
-                                <input id="user_username" type="text" name="user[username]" size="33" placeholder="Username">
-                                <input id="user_password" type="password" name="user[password]" size="33" placeholder="Password">
-                                <label class="checkbox muted hidden-tablet">
-                                <font> Registrarce<font></font></font></label>
-                                <font><font><input class="btn btn-success span3" type="submit" name="commit" value="Entrar"></font></font>
-                            </form>
-
-                        </div>
-                  </li>
-                   
-                </ul>
-            </div>
-        </div>
-      </div>
-    </div>
+	
