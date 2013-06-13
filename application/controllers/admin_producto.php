9796f7699a32df4d9a5ef3dc98e64675fc9787de
David Cid <david@foodland.cl>
2013-06-13 18:43:29 -0400

    cambios admin producto

--- application/controllers/admin_producto.php
+++ application/controllers/admin_producto.php
@@ -2,18 +2,18 @@
 
 class Admin_producto extends CI_Controller {
 
-	
+	public function index()	{
 
-	public function index()
-	{
-		if($this->session->userdata('id')==""){
-			$root= base_url()."admin";
-			header("Location: $root");
-		}else{
+	
+		
 			$this->load->view('templades/header_admin');
 			$this->load->view('pages/admin_producto_view');
-		}
-	
+		
+	}
+	public function nuevo_producto(){
+			$this->load->view('templades/header_admin');
+			$this->load->view('pages/admin_nuevo_producto_view');
+		
 	}
 }
-
+	
