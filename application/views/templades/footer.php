d9faac327d34c30d3d0b1a36714c4bc85fba6922
David Cid <david@foodland.cl>
2013-07-02 01:01:59 -0400

    avance footer

--- application/views/templades/footer.php
+++ application/views/templades/footer.php
@@ -2,45 +2,33 @@
             <div class="container">
                 <div class="row" style="color:white;">
                     <div class="span3">
-                        <br><br>
-                        
                         <img src="<?= base_url();?>img/logo2.png" width="250" height="40"> <sup style="color:white;">2.0</sup>
-
                         <div class="span3" style="font-size:12px; margin-left:0px;">
-                        <p ><br><font><font>Foodland es nuestra empresa emprendedora e innovadora, 
+                        <h5><font><font>Foodland es nuestra empresa emprendedora e innovadora, 
                             formada por un grupo de jóvenes universitarios el año 2012, cuya misión es 
                             mostrar, a través de esta plataforma, una gran gama de locales delivery de 
                             la quinta región (y próximamente en todo Chile) bajo nuestro catálogo, con 
                             el fin de conseguir que nuestros usuarios, de forma práctica y fácil, pidan 
                             los productos que ofrecen nuestros asociados, facilitando de esta forma, la 
-                            conexión entre las personas y los locales.</font></font></p>
+                            conexión entre las personas y los locales.</font></font></h5>
                         <p class="copyright"><font><font>
                             FoodLand © 2013 
                         </font></font></p>
                         </div>
                     </div>
                     <div class="span3 offset1">
-
                         <br><br><br>
                         <h2><font><font>Enlaces </font></font></h2>
                         <ul class="unstyled footer-links">
                             <h4>
                             <li><a href="<?= base_url();?>" class="flink">Inicio</a></li>
-                                <!--
-                            <li><a href="" class="flink"><font><font>Quiénes somos</font></font></a></li>
-                            <li><a href="" class="flink"><font><font>El equipo</font></font></a></li>
-                            <li><a href="" class="flink"><font><font>Contacto</font></font></a></li>
-                            <li><a href="" class="flink"><font><font>Política de privacidad</font></font></a></li>
-                            <li><a href="" class="flink"><font><font>Términos de uso</font></font></a></li>
-                                -->
-                            <li><a href="<?= base_url()?>admin" class="flink"><font><font>Foodland admin</font></font></a></li>
-
-                            
-                            </h4>
-                            
+                            <li><a href="<?= base_url()?>terminos" class="flink"><font><font>Términos y condiciones</font></font></a></li>
+                            <li><a href="<?= base_url()?>nosotros" class="flink"><font><font>Sobre nosotros</font></font></a></li>
+                            <li><a href="<?= base_url();?>tema" class="flink"><font><font>Foodland Team</font></font></a></li>
+                            <li><a href="<?= base_url()?>admin" class="flink"><font><font>Foodland admin</font></font></a></li>                            
+                            </h4>                            
                         </ul>
                     </div>
-
                     <div class="span3 contact ">
                         <br><br><br>
                         <h3 class="center"><font>Contacto</font></h3>
@@ -48,27 +36,22 @@
                             <input type="text" placeholder="Tu nombre">
                             <input type="text" placeholder="Tu Mail">
                             <textarea rows="3" placeholder="Mensaje"></textarea>
-                            <input type="submit" value="Send">
+                            <input type="submit" value="Enviar">
                         </form>
                     </div>
-
+                    <br><br>
                     <div class="span1 offset1">
-                        <br> <br> <br>
+                        <br>
                         <h3><font>Siguenos</font></h3>
                         <br><br>
                         <a href="https://twitter.com/FoodLand_Valpo"><h2 class="center"><i class="icon-twitter"></i></h2></a>
-                        <a href="https://www.facebook.com/FoodLandValparaiso"> <h2 class="center"><i class="icon-facebook"></i></h2></a>
-                        
-                       
+                        <a href="https://www.facebook.com/FoodLandValparaiso"> <h2 class="center"><i class="icon-facebook"></i></h2></a>                                            
                     </div>  
                 </div>
             </div>
 </footer>
 
-<!-- Scripts de Index-->
-
-        <script src="<?= base_url();?>js/jquery-latest.js"></script>
-       
+        <script src="<?= base_url();?>js/jquery-latest.js"></script>       
         <script type="text/javascript" src="<?= base_url();?>js/index-slider.js"></script>  
         <script src="<?= base_url();?>js/modernizr.js"></script>
         <script src="<?= base_url();?>js/bootstrap.js"></script>
@@ -76,14 +59,7 @@
         <script src="<?= base_url();?>js/jquery.isotope.min.js"></script>
         <script src="<?= base_url();?>js/retina.js"></script>
         <script src="<?= base_url();?>js/portfolio.js"></script>
-
-        <!-- Scripts de carousel index-->
-        
         <script src="<?= base_url();?>js/contenido/supersized.3.2.7.min.js"></script>
         <script src="<?= base_url();?>js/contenido/supersized-init.js"></script>
-
-
 <body>
-    <html>
-
-        
+<html>
---
+++ application/views/templades/header_pag_footer.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es" >
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+     <title>Bienvenido a Foodland</title>
+        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/theme.css">
+        <link href="<?= base_url();?>css/style-responsive.css" rel="stylesheet" type="text/css">
+        <link href="<?= base_url();?>css/bootstrap.css" rel="stylesheet" type="text/css">
+
+        <link rel="stylesheet" href="<?= base_url();?>css/style.css" type="text/css">
+        <link rel="stylesheet" href="<?= base_url();?>css/style_terminos.css">
+</head>
+
+<body style="background:#c9c9c9">
+    <div class="navbar transparent navbar-inverse navbar-fixed-top" style="height:45px;">
+      <div class="navbar-inner" style="height:45px;">
+         <a class="span3 offset2" style="position:relative; left:20px;" href="<?= base_url();?>">
+                <img src="<?= base_url();?>img/logo2.png" width="200" height="40"> <sup style="color:white;">BETA</sup>
+            </a>     
+        <div class="container">
+          <div class="nav-collapse">
+                <ul class="nav pull-right" style="position:relavite; top:-10px;">                        
+                        <li><a href="<?= base_url();?>" class="active">Inicio</a></li>
+                        <li><a href="#portfolio">Categoria Productos</a></li>
+                        <li class="dropdown">
+                            <a href="" class="dropdown-toggle" data-toggle="dropdown">
+                                Sector reparto
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <?php foreach ($sector_entregas as $s) {
+                                ?>    
+                                <li><a href="<?= base_url();?>home?sector=<?= $s->id_sector_entrega  ?>"><?= $s->nombre_sector_entrega ?></a></li>
+                                <?php }?>
+                            </ul>
+                        </li>                                                           
+                   <li class="dropdown">
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">
+                            <font><font>
+                                 <?php if($this->session->userdata('user_profile')) : 
+                                        $usuario= $this->session->userdata('user_profile');
+                                        echo $usuario['first_name'];
+                                    ?>
+
+                                    <?php else : ?>
+                                    Iniciar Session
+                                    <?php endif; ?>
+                            </font></font><b class="caret"></b>
+                        </a>
+                        <div class="dropdown-menu" style="padding: 15px; color:white;" >
+                            <div class="span3">
+                                <div >
+                                    <?php if($this->session->userdata('user_profile')) : 
+                                        $usuario= $this->session->userdata('user_profile');
+                                        
+                                    ?>
+                                     <div class="logout">
+                                        <p><a href="<?php echo $logout_url; ?>">Cerrar Session</a></p>
+                                    </div>
+                                    <?php else : ?>
+                                    <font><font>Iniciar con:</font></font>
+                                        <a href="<?php echo $login_url; ?>"><img src="<?= base_url();?>img/icono-facebook.png" width="40px"></a>                                                                     
+                                    <?php endif; ?>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                  </li>                
+                </ul>
+            </div>
+        </div>
+      </div>
+    </div>
