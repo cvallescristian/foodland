2147c7d8f08ff9f09463d30f13031d11c7b08fba
David Cid <david@foodland.cl>
2013-07-07 13:10:40 -0400

    Resubir cambios de pag footer
    se habian eliminado mis cambios

--- application/views/pages/foodland_team_view.php
+++ application/views/pages/foodland_team_view.php
@@ -1,58 +1,43 @@
-	<div class="container margen">
-		<div class="bloque_blanco">
-		<div class="hero-unit" class="bloque_blanco bloque_b">
-			<h1>Nuestro Equipo</h1>
-			<h4>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO </h4>
-		</div>
-		</div>
-		<div class="container">
-			<div class="span4 bloque_blanco box">
-			<h3> Cristian Valles</h3>
-				<img src="img/temp.jpg" class="img-circle img">
-				<p class="text">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
+	<div class="row">
+		<div class="offset6 span5">
+			<div class="bloque_blanco">
+				<h1 style="text-align:center">Equipo Foodland</h1>
 			</div>
-			<div class="span4 bloque_blanco box">
-				<h3>Diego Reyes</h3>
-				<img src="img/temp.jpg" class="img-circle img">
-				<p class="text">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
-		   </div>
-		   <div class="span4 bloque_blanco box">
-				<h3>David Cid</h3>
-				<img src="img/temp.jpg" class="img-circle img">
-				<p class="text">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
+ 		</div>
+	</div>
+	<div class="row">
+		<div class="offset2">
+			<div class="span2 bloque_blanco">
+				<h3 style="text-align:center">Cristian Valles</h3>
+				<img src="img/temp.jpg" class="img-circle">
+				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
+	 		</div>
+			<div class="span2 bloque_blanco">
+				<h3 style="text-align:center">Diego Reyes</h3>
+				<img src="img/temp.jpg" class="img-circle">
+				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
+ 		   </div>
+
+		   <div class="span2 bloque_blanco">
+				<h3 style="text-align:center">David Cid Castro</h3>
+				<img src="img/temp.jpg" class="img-circle">
+				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
 			</div>
-		</div>
-		<br>
-		<div class="container">
-			<div class="span4 bloque_blanco box">
-				<h3>Christian Cortéz</h3>
-				<img src="img/temp.jpg" class="img-circle img">
-				<p class="text">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
-			</div>
-			<div class="span4 bloque_blanco box">
-				<h3>Jonathan Galassi</h3>
-				<img src="img/temp.jpg" class="img-circle img">
-				<p class="text">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
-		   </div>
-<<<<<<< HEAD
+			<div class="span2 bloque_blanco">
+				<h3 style="text-align:center">Jonathan Galassi</h3>
+				<img src="img/temp.jpg" class="img-circle">
+				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
+		   	</div>
+		   	<div class="span2 bloque_blanco">
+				<h3 style="text-align:center">Carlos Bugeño</h3>
+				<img src="img/temp.jpg" class="img-circle">
+				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
+		   	</div>
 			<div class="span2 bloque_blanco">
 				<h3 style="text-align:center">NN</h3>
 				<img src="img/temp.jpg" class="img-circle">
 				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
-=======
-			<div class="span4 bloque_blanco box">
-				<h3>Pablo Herrera</h3>
-				<img src="img/temp.jpg" class="img-circle img">
-				<p class="text">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
->>>>>>> parent of 65d5474... Merge branch 'tester' of https://github.com/stilexmagic/foodland into tester
 			</div>
 		</div>
-		<br>
-		<div class="offset4 container">
-			<div class="span4 bloque_blanco box">
-				<h3>Carlos Bugueño</h3>
-				<img src="img/temp.jpg" class="img-circle img">
-				<p class="text">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
-		   </div>
-		</div>
+		<br>		
 	</div> <!-- /container -->
