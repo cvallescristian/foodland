3d945c6f46cc2f73087b489f94ec30dfb96e312c
David Cid <david@foodland.cl>
2013-07-07 13:35:51 -0400

    agrega descripcion a team

--- application/views/pages/foodland_team_view.php
+++ application/views/pages/foodland_team_view.php
@@ -6,38 +6,32 @@
  		</div>
 	</div>
 	<div class="row">
-		<div class="offset2">
+		<div class="offset3">
 			<div class="span2 bloque_blanco">
 				<h3 style="text-align:center">Cristian Valles</h3>
 				<img src="img/temp.jpg" class="img-circle">
-				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
+				<h4 class="text" style="text-align:center">CEO y desarrollador back-end. <br><br>cristian@<br>foodland.cl</h4>
 	 		</div>
 			<div class="span2 bloque_blanco">
 				<h3 style="text-align:center">Diego Reyes</h3>
 				<img src="img/temp.jpg" class="img-circle">
-				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
- 		   </div>
-
-		   <div class="span2 bloque_blanco">
+				<h4 class="text" style="text-align:center">Desarrollador front-end del sitio. <br><br>diego@<br>foodland.cl</h4>
+ 		   	</div>
+			<div class="span2 bloque_blanco">
+				<h3 style="text-align:center">Carlos Bugeño</h3>
+				<img src="img/temp.jpg" class="img-circle">
+				<h4 class="text" style="text-align:center">Jefe de marketing y diseñador. <br><br>carlos@<br>foodland.cl</h4>
+		   	</div>
+		   	<div class="span2 bloque_blanco">
 				<h3 style="text-align:center">David Cid Castro</h3>
 				<img src="img/temp.jpg" class="img-circle">
-				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
+				<h4 class="text" style="text-align:center">Desarrollador front-end del sitio. <br><br>david@<br>foodland.cl</h4>
 			</div>
 			<div class="span2 bloque_blanco">
 				<h3 style="text-align:center">Jonathan Galassi</h3>
 				<img src="img/temp.jpg" class="img-circle">
-				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
-		   	</div>
-		   	<div class="span2 bloque_blanco">
-				<h3 style="text-align:center">Carlos Bugeño</h3>
-				<img src="img/temp.jpg" class="img-circle">
-				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
-		   	</div>
-			<div class="span2 bloque_blanco">
-				<h3 style="text-align:center">NN</h3>
-				<img src="img/temp.jpg" class="img-circle">
-				<p class="text" style="text-align:center">DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN DESCRIPCIÓN.</p>
-			</div>
+				<h4 class="text" style="text-align:center">Desarrollador back-end del sitio. <br><br>jonathan@<br>foodland.cl</h4>
+		   	</div>		   	
 		</div>
 		<br>		
 	</div> <!-- /container -->
