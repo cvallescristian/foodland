bb364478fb72c96b1a8be86eec5228d4160180b4
Cristian <cristian.valles.12@gmail.com>
2013-07-17 00:58:53 -0400

    reduccion de imagenes desde la administracion

--- application/controllers/admin_local.php
+++ application/controllers/admin_local.php
@@ -69,6 +69,7 @@ class Admin_local extends CI_Controller {
 			$this->load->model('admin_model','uum');
 			$id_local_nuevo= $this->uum->agregar_local($data);
 	  		move_uploaded_file($_FILES['userfile']['tmp_name'],"img/locales/$id_local_nuevo"."_logo.png");
+	  		reducir_imagen_logo_local($id_local_nuevo);
 		 		$root= base_url()."admin_local?al=1";
   				echo "<script>location.href='$root';</script>";	
 
@@ -87,6 +88,7 @@ class Admin_local extends CI_Controller {
 			$id=$this->input->get('id');
 			$this->load->model('admin_model','uum');
 			$this->uum->borrar_local($id);
+			imagedestroy(base_url().'img/locales/'.$id.'logo.png');
 			$root= base_url()."admin_local";
     		echo "<script>location.href='$root';</script>";
 		}
@@ -196,7 +198,10 @@ class Admin_local extends CI_Controller {
 			$root= base_url()."admin_local";
 			echo "<script>location.href='$root';</script>";	 
 			$id_local_editar = $_GET['id'];
-	  	move_uploaded_file($_FILES['userfile']['tmp_name'],"img/locales/$id_local_editar _logo.png");
+	  		move_uploaded_file($_FILES['userfile']['tmp_name'],"img/locales/$id_local_editar _logo.png");
+	  		if($_FILES['userfile']['tmp_name']!=""){
+	  			reducir_imagen_logo_local($id_local_editar);
+	  		}
 		 	//	$root= base_url()."admin_local?al=1";
   			//	echo "<script>location.href='$root';</script>";	
 			$this->uum->eliminar_sector_local($_GET['id']);
