1e73cc2c9ea73c2bfe5dc02e46b76f13d69b58ba
Cristian <cristian.valles.12@gmail.com>
2013-07-19 23:49:35 -0400

    reduccion de fotos funcionando

--- application/controllers/admin_producto.php
+++ application/controllers/admin_producto.php
@@ -4,6 +4,13 @@ class Admin_producto extends CI_Controller {
 
 	public function index()	{
 
+			$info=$this->input->get('info');
+			$data['info']="";
+			if($info==1){
+				$data['info']="Producto Agregado Exitosamente";
+			}else if($info==2){
+				$data['info']=" Tipo de producto Agregado Exitosamente";
+			}
 			$this->load->model('admin_model','uum');
 
 			$producto = $this->uum->get_producto();
@@ -48,10 +55,35 @@ class Admin_producto extends CI_Controller {
 
 		$this->load->model('admin_model','uum');
 		$id_producto_nuevo= $this->uum->agregar_producto($data);
+		$extension = explode("/",$_FILES['userfile']['type']);
+		move_uploaded_file($_FILES['userfile']['tmp_name'],"img/locales/".$id_producto_nuevo.".".$extension[1]);
 
-		move_uploaded_file($_FILES['userfile']['tmp_name'],"img/locales/".$id_producto_nuevo.".png");
-
-		 $root= base_url()."admin_producto";
+		//inicio reduccion de la foto
+		$id=$id_producto_nuevo;
+		echo $extension[1];
+		if($extension[1]=="jpeg" || $extension[1]=="jpg"){
+    		$img_origen = imagecreatefromjpeg( base_url().'img/locales/'.$id.'.'.$extension['1'] );
+    	}else if($extension[1]=="png"){
+			$img_origen = imagecreatefrompng( base_url().'img/locales/'.$id.'.'.$extension['1'] );
+    	}
+		$ancho_origen = imagesx( $img_origen );//se ontiene el ancho de la imagen
+		$alto_origen = imagesy( $img_origen );//se obtiene el alto de la imagen
+		$ancho_limite=720;
+		if($ancho_origen>$alto_origen){// para foto horizontal
+		    
+		    $ancho_origen=$ancho_limite;
+		    $alto_origen=$ancho_limite*imagesy( $img_origen )/imagesx( $img_origen );
+		    
+		}else{//para fotos verticales
+		    $alto_origen=$ancho_limite;
+		    $ancho_origen=$ancho_limite*imagesx( $img_origen )/imagesy( $img_origen );
+		}
+		$img_destino = imagecreatetruecolor($ancho_origen ,$alto_origen );// se crea la imagen segun las dimensiones dadas
+		// copy/resize as usual
+		imagecopyresized( $img_destino, $img_origen, 0, 0, 0, 0, $ancho_origen, $alto_origen, imagesx( $img_origen ), imagesy( $img_origen ) );
+		imagejpeg( $img_destino, "img/locales/".$id.".jpeg"); //se guarda la nueva foto 
+		//termino reduccion de la foto
+		 $root= base_url()."admin_producto?info=1";
   		echo "<script>location.href='$root';</script>";
     }
 	public function editar_producto(){
@@ -87,12 +119,40 @@ class Admin_producto extends CI_Controller {
     	$config['max_size'] = '100'; 
 
 		$this->load->model('admin_model','uum');
-		$id_producto_nuevo= $this->uum->editar_producto($data,$_GET['id']);
+		$this->uum->editar_producto($data,$_GET['id']);
+		$id_producto_nuevo= $_GET['id'];
+		echo $id_producto_nuevo;
+		$extension = explode("/",$_FILES['userfile']['type']);
+		move_uploaded_file($_FILES['userfile']['tmp_name'],"img/locales/".$id_producto_nuevo.".".$extension[1]);
 
-		move_uploaded_file($_FILES['userfile']['tmp_name'],"img/locales/".$_GET['id'].".jpeg");
-
-		 $root= base_url()."admin_producto";
+		//inicio reduccion de la foto
+		$id=$id_producto_nuevo;
+		echo $extension[1];
+		if($extension[1]=="jpeg" || $extension[1]=="jpg"){
+    		$img_origen = imagecreatefromjpeg( base_url().'img/locales/'.$id.'.'.$extension['1'] );
+    	}else if($extension[1]=="png"){
+			$img_origen = imagecreatefrompng( base_url().'img/locales/'.$id.'.'.$extension['1'] );
+    	}
+		$ancho_origen = imagesx( $img_origen );//se ontiene el ancho de la imagen
+		$alto_origen = imagesy( $img_origen );//se obtiene el alto de la imagen
+		$ancho_limite=720;
+		if($ancho_origen>$alto_origen){// para foto horizontal
+		    
+		    $ancho_origen=$ancho_limite;
+		    $alto_origen=$ancho_limite*imagesy( $img_origen )/imagesx( $img_origen );
+		    
+		}else{//para fotos verticales
+		    $alto_origen=$ancho_limite;
+		    $ancho_origen=$ancho_limite*imagesx( $img_origen )/imagesy( $img_origen );
+		}
+		$img_destino = imagecreatetruecolor($ancho_origen ,$alto_origen );// se crea la imagen segun las dimensiones dadas
+		// copy/resize as usual
+		imagecopyresized( $img_destino, $img_origen, 0, 0, 0, 0, $ancho_origen, $alto_origen, imagesx( $img_origen ), imagesy( $img_origen ) );
+		imagejpeg( $img_destino, "img/locales/".$id.".jpeg"); //se guarda la nueva foto 
+		//termino reduccion de la foto
+		 $root= base_url()."admin_producto?info=1";
   		echo "<script>location.href='$root';</script>";
+		
     }
     public function borrar_producto(){
     	$id = $this->input->get('id');
@@ -128,10 +188,38 @@ class Admin_producto extends CI_Controller {
 	    	$config['max_size'] = '100'; 
 
 	    	$this->load->model('admin_model','uum');
-			$id_tipo_producto_nuevo= $this->uum->agregar_tipo_producto($data);
-	  		move_uploaded_file($_FILES['userfile']['tmp_name'],"img/tipo_producto/$id_tipo_producto_nuevo.jpeg");
-		 		$root= base_url()."admin_producto?al=1";
-  				echo "<script>location.href='$root';</script>";	
+			$id_producto_nuevo= $this->uum->agregar_tipo_producto($data);
+	  		$extension = explode("/",$_FILES['userfile']['type']);
+		move_uploaded_file($_FILES['userfile']['tmp_name'],"img/tipo_producto/".$id_producto_nuevo.".".$extension[1]);
+
+		//inicio reduccion de la foto
+		$id=$id_producto_nuevo;
+		echo $extension[1];
+		if($extension[1]=="jpeg" || $extension[1]=="jpg"){
+    		$img_origen = imagecreatefromjpeg( base_url().'img/tipo_producto/'.$id.'.'.$extension['1'] );
+    	}else if($extension[1]=="png"){
+			$img_origen = imagecreatefrompng( base_url().'img/tipo_producto/'.$id.'.'.$extension['1'] );
+    	}
+		$ancho_origen = imagesx( $img_origen );//se ontiene el ancho de la imagen
+		$alto_origen = imagesy( $img_origen );//se obtiene el alto de la imagen
+		$ancho_limite=720;
+		if($ancho_origen>$alto_origen){// para foto horizontal
+		    
+		    $ancho_origen=$ancho_limite;
+		    $alto_origen=$ancho_limite*imagesy( $img_origen )/imagesx( $img_origen );
+		    
+		}else{//para fotos verticales
+		    $alto_origen=$ancho_limite;
+		    $ancho_origen=$ancho_limite*imagesx( $img_origen )/imagesy( $img_origen );
+		}
+		$img_destino = imagecreatetruecolor($ancho_origen ,$alto_origen );// se crea la imagen segun las dimensiones dadas
+		// copy/resize as usual
+		imagecopyresized( $img_destino, $img_origen, 0, 0, 0, 0, $ancho_origen, $alto_origen, imagesx( $img_origen ), imagesy( $img_origen ) );
+		imagepng( $img_destino, "img/tipo_producto/".$id.".png"); //se guarda la nueva foto 
+		//termino reduccion de la foto
+		
+	  		$root= base_url()."admin_producto?info=2";
+  			echo "<script>location.href='$root';</script>";	
 		}
     }
     public function borrar_tipo_producto(){
@@ -146,6 +234,6 @@ class Admin_producto extends CI_Controller {
     		//echo "<script>location.href='$root';</script>";
     	}
     }
-
+    
 }
 	
