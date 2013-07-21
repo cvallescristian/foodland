5459daf7da3c30aeca28e6b5686d102fbe974b2c
diieegoo01 <diiegoreyes@gmail.com>
2013-07-20 22:15:41 -0400

    Nuevo diseño del detalle producto
    falta darle funcionalidad al pop-up, y pequeños detalles de diseño.

---
+++ application/views/pages/locales.php
@@ -0,0 +1,244 @@
+
+<!-- Main Area -->
+<div class="main-area">
+	<div class="container">
+		<div class="row">
+			<div class="span8">
+				
+				<div class="post">
+					<div id="myCarousel" class="carousel slide">
+					  <!-- Carousel items -->
+					  <div class="carousel-inner">
+					    <div class="active item">
+					    	<a href="">
+					    		<img src="<?=base_url();?>img/logo-locales/5.png" alt="Placeholder">
+					    		<span class="strapline">Akipe<br><span>A short description of what the post is about.</span></span>
+					    	</a>
+					    </div>
+					    <div class="item">
+					    	<a href="">
+					    		<img src="<?=base_url();?>img/logo-locales/5.png" alt="Placeholder">
+					    		<span class="strapline">Nombre Local<br><span>A short description of what the post is about.</span></span>
+					    	</a>
+					    </div>
+					    <div class="item">
+					    	<a href="">
+					    		<img src="<?=base_url();?>img/logo-locales/5.png" alt="Placeholder">
+					    		<span class="strapline">Nombre Local<br><span>A short description of what the post is about.</span></span>
+					    	</a>
+					    </div>
+					   
+					  </div>
+					  <!-- Carousel nav -->
+					  <a class="carousel-control left" href="locales.html#myCarousel" data-slide="prev">‹</a>
+					  <a class="carousel-control right" href="locales.html#myCarousel" data-slide="next">›</a>
+					</div>
+				</div>
+				
+				<div class="row">
+					
+					<div class="span4">
+						<div class="post">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/1.png" alt="Placeholder"></a>
+							<div class="summary">
+								<h2><a href="">MetPizza</a></h2>
+								<p>Breve descripcion</p>
+							</div>
+							
+						</div>
+					</div>
+					
+					<div class="span4">
+						<div class="post">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/5.png" alt="Placeholder"></a>
+							<div class="summary">
+								<h2><a href="">Nombre Local</a></h2>
+								<p>An example of a post template, with content styles and comments</p>
+							</div>
+							
+						</div>
+					</div>
+					
+				</div>
+				
+				<div class="row">
+					
+					<div class="span4">
+						<div class="post">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/5.png" alt="Placeholder"></a>
+							<div class="summary">
+								<h2><a href="">Nombre Local</a></h2>
+								<p>An example of a post template, with content styles and comments</p>
+							</div>
+						
+						</div>
+					</div>
+					
+					<div class="span4">
+						<div class="post">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/1.png" alt="Placeholder"></a>
+							<div class="summary">
+								<h2><a href="">Nombre Local</a></h2>
+								<p>An example of a post template, with content styles and comments</p>
+							</div>
+							
+						</div>
+					</div>
+					
+				</div>
+				
+				<div class="row">
+					
+					<div class="span4">
+						<div class="post">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/5.png" alt="Placeholder"></a>
+							<div class="summary">
+								<h2><a href="">Nombre Local</a></h2>
+								<p>An example of a post template, with content styles and comments</p>
+							</div>
+							
+						</div>
+					</div>
+					
+					<div class="span4">
+						<div class="post">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/1.png" alt="Placeholder"></a>
+							<div class="summary">
+								<h2><a href="">Nombre Local</a></h2>
+								<p>An example of a post template, with content styles and comments</p>
+							</div>
+						
+						</div>
+					</div>
+					
+				</div>
+				
+				<div class="row">
+					
+					<div class="span4">
+						<div class="post">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/1.png" alt="Placeholder"></a>
+							<div class="summary">
+								<h2><a href="">Nombre Local</a></h2>
+								<p>An example of a post template, with content styles and comments</p>
+							</div>
+							
+						</div>
+					</div>
+					
+					<div class="span4">
+						<div class="post">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/5.png" alt="Placeholder"></a>
+							<div class="summary">
+								<h2><a href="">Nombre Local</a></h2>
+								<p>An example of a post template, with content styles and comments</p>
+							</div>
+							
+						</div>
+					</div>
+					
+				</div>
+				
+				<div class="pagination pull-right">
+				  <ul>
+				    <li><a href="">Anterior</a></li>
+				    <li><a href="">1</a></li>
+				    <li><a href="">2</a></li>
+				    <li><a href="">3</a></li>
+				    <li><a href="">4</a></li>
+				    <li><a href="">Siguiente</a></li>
+				  </ul>
+				</div>
+				
+			</div>
+			
+			<div class="span4 sidebar">
+				<form class="form-inline well">
+					<div class="input-append">
+						<input class="span3" size="16" type="text" placeholder="Buscar Local"><button class="btn btn-primary" type="button">Ir</button>
+					</div>
+				</form>
+				
+				
+				<div class="row">
+					<div class="span2 side-nav">
+						<ul class="nav nav-tabs nav-stacked">
+							<li class="title"><a href="">Por Categoria</a></li>
+							<li><a href="">Sushi</a></li>
+							<li><a href="">Pizza</a></li>
+							<li><a href="">Comida China</a></li>
+							<li><a href="">Sandwich</a></li>
+							<li><a href="">Arabe</a></li>
+							<li><a href="">Comida Rapida</a></li>
+						</ul>
+					</div>
+					<div class="span2 side-nav">
+						<ul class="nav nav-tabs nav-stacked">
+							<li class="title"><a href="">Por Ubicación</a></li>
+							<li><a href="">Valparaiso</a></li>
+							<li><a href="">Viña del mar</a></li>
+							<li><a href="">Reñaca</a></li>
+							<li><a href="">Quilpue</a></li>
+							<li><a href="">Villa Alemana</a></li>
+							
+						</ul>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="span2">
+						<div class="ad">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/1.png" alt="Placeholder"></a>
+						</div>
+					</div>
+					<div class="span2">
+						<div class="ad">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/4.png" alt="Placeholder"></a>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="span2">
+						<div class="ad">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/4.png" alt="Placeholder"></a>
+						</div>
+					</div>
+					<div class="span2">
+						<div class="ad">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/1.png" alt="Placeholder"></a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="span4">
+						<div class="ad">
+							<a href=""><img src="<?= base_url();?>img/logo-locales/5.png" alt="Placeholder"></a>
+						</div>
+					</div>
+				</div>
+				
+				
+				
+				<div class="row">
+					<div class="span4">
+						<h2>Twitter Feed</h2>
+						<div class="twitter-feed"><p class="loading">loading tweets...</p></div>
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+<!-- End Main Area -->
+</div>
+
+
+
+<!-- Javascript -->
+<script src="<?= base_url();?>js/js-locales/jquery.min.js"></script>
+<script src="<?= base_url();?>js/js-locales/bootstrap.js"></script>
+<script src="<?= base_url();?>js/js-locales/jquery.tweet.js"></script>
+<script src="<?= base_url();?>js/js-locales/jflickrfeed.min.js"></script>
+<script src="<?= base_url();?>js/js-locales/magazine.js"></script>
---
+++ application/views/templades/header_producto.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <link rel="shortcut icon" href="img/logo.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+     <title>Bienvenido a Foodland</title>
+        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/theme.css">
+        <link href="<?= base_url();?>css/css.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="<?= base_url();?>css/home.css"> 
+        <link rel="stylesheet" href="<?= base_url();?>css/font-awesome.min.css" rel="stylesheet">   
+    
+        <link href="<?= base_url();?>css/font-awesome.css" media="all" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/jquery.fancybox.css" media="all">
+        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/jquery.fancybox-thumbs.css">
+        <link href="<?= base_url();?>css/animate.css" rel="stylesheet" type="text/css">
+        <link href="<?= base_url();?>css/isotope.css" rel="stylesheet" type="text/css">
+        <link href="<?= base_url();?>css/style.css" rel="stylesheet" type="text/css">        
+      
+        <link href="<?= base_url();?>css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="<?= base_url()?>css/header/header.css">
+        <link rel="stylesheet" href="<?= base_url();?>css/home.css"> 
+        <link rel="stylesheet" href="<?= base_url();?>css/style_footer.css">
+        <link rel="stylesheet" href="<?= base_url();?>css/fondocarousel.css">
+
+        <link rel="stylesheet" href="<?= base_url();?>css/css_productos2/style.css"> 
+        <link rel="stylesheet" href="<?= base_url();?>css/css_productos2/bootstrap.css"> 
+
+</head>
+<body>
+    <div class="navbar transparent navbar-inverse navbar-fixed-top" style="height:45px;">
+      <div class="navbar-inner" >
+         <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+         </a>      
+        <div class="span3">
+             <a  id="menu" href="<?= base_url();?>">
+                    <img src="<?= base_url();?>img/logo2.png" width="200" height="40"> <sup style="color:white;">BETA</sup>
+             </a>   
+         </div>
+         <div class="span2">
+            <form class="form-search" action="<?= base_url()?>producto/buscar" method="post">
+              <div class="input-append" id="buscador">
+                <input type="text" name="search" id="search" class="search-query" placeholder="¿Qué quieres comer?" autocomplete="off" required>
+                <button type="submit"  class="btn btn-warning"><i class="icon icon-search"></i></button>
+              </div>
+              
+            </form>
+         </div>
+          
+            
+          <div class="nav-collapse collapse" cz-shortcut-listen="true">
+                <ul class="nav pull-right"> 
+                        <li><a href="<?= base_url();?>">Inicio</a></li>
+                        <li><a href="<?= base_url()?>nuestros_locales">Nuestros Locales</a></li>
+                        <li class="dropdown">
+                            <a href="" class="dropdown-toggle" data-toggle="dropdown">
+                                Sector reparto
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <?php foreach ($sector_entregas as $s) {
+                                ?>    
+                                <li><a href="<?= base_url();?>home?sector=<?= $s->id_sector_entrega  ?>"><?= $s->nombre_sector_entrega ?></a></li>
+                                <?php }?>
+                            </ul>
+                        </li>                        
+                   <li class="dropdown">
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">
+                            <font><font>
+                                 <?php if($this->session->userdata('user_profile')) : 
+                                        $usuario= $this->session->userdata('user_profile');
+                                        echo $usuario['first_name'];
+                                    ?>
+
+                                    <?php else : ?>
+                                    Iniciar Sesion
+                                    <?php endif; ?>
+                            </font></font><b class="caret"></b>
+                        </a>
+                        <div class="dropdown-menu" style="padding: 15px; color:white;" >
+                            <div class="span3">
+                                <div >
+                                    <?php if($this->session->userdata('user_profile')) : 
+                                        $usuario= $this->session->userdata('user_profile');
+                                        
+                                    ?>
+                                     <div class="logout">
+                                        <p><a href="<?php echo $logout_url; ?>">Cerrar Sesion</a></p>
+                                    </div>
+                                    <?php else : ?>
+                                    <font><font>Iniciar con:</font></font>
+                                        <a href="<?php echo $login_url; ?>"><img src="<?= base_url();?>img/icono-facebook.png" width="40px"></a>                                                                     
+                                    <?php endif; ?>                                   
+                                </div>
+                            </div>
+                        </div>
+                    </li>                    
+                </ul>
+            </div>
+       </div>
+    </div>
