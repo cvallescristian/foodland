92f3ff482a1c4bec36ea42f238a994cfa3b9a730
David Cid <david@foodland.cl>
2013-07-25 19:57:28 -0400

    solucionado fondo admin login

--- application/views/templades/header_logo.php
+++ application/views/templades/header_logo.php
@@ -1,38 +1,24 @@
 <!DOCTYPE html>
-<!-- saved from url=(0049)http://jumpstartuidemo.com/themes/base/index.html -->
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <title>Foodland Admin</title>    
         <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/theme.css">
-        <link href="<?= base_url();?>css/css.css" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="http://responsivewebinc.com/premium/metroman/style/font-awesome.css">
-        <link rel="stylesheet" href="<?= base_url();?>css/slider-main.css">       
-        <link href="<?= base_url();?>css/font-awesome.css" media="all" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/jquery.fancybox.css" media="all">
-        <link rel="stylesheet" type="text/css" href="<?= base_url();?>css/jquery.fancybox-thumbs.css">
-        <link href="<?= base_url();?>css/animate.css" rel="stylesheet" type="text/css">
-        <link href="<?= base_url();?>css/isotope.css" rel="stylesheet" type="text/css">
-        <link href="<?= base_url();?>css/style.css" rel="stylesheet" type="text/css">        
+        <link href="<?= base_url();?>css/css.css" rel="stylesheet" type="text/css">        
         <link href="<?= base_url();?>css/bootstrap.css" rel="stylesheet" type="text/css">
         <link href="<?= base_url();?>css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="<?= base_url();?>css/home.css">         
         <link rel="stylesheet" href="<?= base_url();?>css/style_admin.css">
-        <link rel="stylesheet" href="<?= base_url();?>css/fondocarousel.css">
 </head>
 <body>
-<div class="navbar navbar-inverse navbar-fixed-top">	<!--Parte 1 Header -->		
+<div class="navbar navbar-inverse navbar-fixed-top">
 	<div class="navbar-inner">		
 		<div class="container">			
-			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<i class="icon-cog"></i>
-			</a>			
 			<a class="span4">
 				<img src="img/logo2.png" width="200" height="40"> <sup style="color:white;">Beta</sup>
 			</a>			
 		</div> 
 	</div>
-</div> <!--/Parte 1 Header -->		
+</div>
 </body>
 </html>
