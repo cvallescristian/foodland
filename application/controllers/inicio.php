ef4dee5457101d0881dbade294189eb3e64d95da
David Cid <david@foodland.cl>
2013-10-11 14:52:19 -0200

    index, error url

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -1,9 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Perfil extends CI_Controller {
+class Inicio extends CI_Controller {
 
 	public function index()	{
 
-			$this->load->view('pages/perfil/perfil_view');
+			$this->load->view('pages/index_view');
 	}
 }
